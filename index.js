b7ae8af0ac220b824e395ee7cc7ea2e57a3f8458
daiju81 <1d07c.m8k.prv@gmail.com>
2020-05-11 14:11:34 +0900

    queryの利用

--- index.js
+++ index.js
@@ -9,9 +9,23 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   var msg =
     'This is Express Page!<br>' + 'これは、スタイルシートを利用した例です';
+  var url = '/other?name=taro&pass=yamada';
   res.render('index.ejs', {
     title: 'Index',
     content: msg,
+    link: { href: url, text: '※別のページに移動' },
+  });
+});
+
+app.get('/other', (req, res) => {
+  var name = req.query.name;
+  var pass = req.query.pass;
+  var msg =
+    'あなたの名前は「' + name + '」<br>パスワードは「' + pass + '」です';
+  res.render('index.ejs', {
+    title: 'other',
+    content: msg,
+    link: { href: '/', text: '※トップに戻る' },
   });
 });
 
