3725f42401fb89d978af43b4d8eb9cc995e792f5
daiju81 <1d07c.m8k.prv@gmail.com>
2020-05-11 11:45:37 +0900

    Init

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var ejs = require('ejs');
+
+var app = express();
+
+app.engine('ejs', ejs.renderFile);
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  var msg =
+    'This is Express Page!<br>' + 'これは、スタイルシートを利用した例です';
+  res.render('index.ejs', {
+    title: 'Index',
+    content: msg,
+  });
+});
+
+var server = app.listen(3000, () => {
+  console.log('Server is running');
+});
